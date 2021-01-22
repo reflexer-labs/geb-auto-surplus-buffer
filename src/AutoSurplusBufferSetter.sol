9dc26bdea03db5a109e1eea2811af2de6778bf48
stefanionescu <stefanionescu@protonmail.com>
2021-01-22 22:21:12 +0000

    Update deps

--- src/AutoSurplusBufferSetter.sol
+++ src/AutoSurplusBufferSetter.sol
@@ -66,9 +66,6 @@ contract AutoSurplusBufferSetter is IncreasingTreasuryReimbursement {
     function both(bool x, bool y) internal pure returns (bool z) {
       assembly{ z := and(x, y)}
     }
-    function either(bool x, bool y) internal pure returns (bool z) {
-        assembly{ z := or(x, y)}
-    }
 
     // --- Administration ---
     function modifyParameters(bytes32 parameter, uint256 val) external isAuthorized {
